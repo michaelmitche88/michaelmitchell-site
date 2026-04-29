88720702925c4e7834c6f0faa5878f59dbc0cf03
michaelmitche88 <michael.mitche@gmail.com>
2026-04-29 11:50:02 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,15 @@ export const metadata: Metadata = {
     type: "website",
     url: "https://michaelmitchell.tech",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
